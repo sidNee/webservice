824bfbcf3cd031e9e1f24565d044f3b6f4dd3e49
sidNee <lilcashsid@hotmail.de>
2015-10-30 11:49:56 +0100

    Ende 30.10.2015

--- Calculator/CalcService/Calculator.cs
+++ Calculator/CalcService/Calculator.cs
@@ -13,13 +13,14 @@ namespace CalcService
             //TODO Authorisierung
 
             var context= requestIndentity.Impersonate();
+
             var threadIndentity2 = WindowsIdentity.GetCurrent();
             //TODO Auf resourcen mit der request Indentity zugreifen
 
             Console.WriteLine("request:{0}, thread:{1}", requestIndentity.Name, threadIndentity.Name);
-            Console.WriteLine("request:{0}, thread:{1}", requestIndentity.Name, threadIndentity2.Name);
+            Console.WriteLine("request:{0}, thread2:{1}", requestIndentity.Name, threadIndentity2.Name);
 
-            context.Undo();
+            //context.Undo();
 
             Result result = new Result();
             result.Value = args.Arg1 + args.Arg2;
