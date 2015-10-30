abf53f3b8706936777d27c9cac8adf01c8e7fbc9
sidNee <lilcashsid@hotmail.de>
2015-10-30 11:45:07 +0100

    Identity rumspielerei (request & thread )

--- Calculator/CalcService/Calculator.cs
+++ Calculator/CalcService/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Principal;
+using System.ServiceModel;
 
 namespace CalcService
 {
@@ -6,6 +8,19 @@ namespace CalcService
     {
         public Result Add(Arguments args)
         {
+            var requestIndentity = ServiceSecurityContext.Current.WindowsIdentity;
+            var threadIndentity  = WindowsIdentity.GetCurrent();
+            //TODO Authorisierung
+
+            var context= requestIndentity.Impersonate();
+            var threadIndentity2 = WindowsIdentity.GetCurrent();
+            //TODO Auf resourcen mit der request Indentity zugreifen
+
+            Console.WriteLine("request:{0}, thread:{1}", requestIndentity.Name, threadIndentity.Name);
+            Console.WriteLine("request:{0}, thread:{1}", requestIndentity.Name, threadIndentity2.Name);
+
+            context.Undo();
+
             Result result = new Result();
             result.Value = args.Arg1 + args.Arg2;
             return result;
