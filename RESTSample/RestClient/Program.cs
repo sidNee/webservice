8e8c3fb69330e4e6ac4494033174fb2f1ca3a535
sidNee <lilcashsid@hotmail.de>
2015-12-11 10:44:35 +0100

    REST

---
+++ RESTSample/RestClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var task = client.GetAsync("http://localhost:5000/api/Customers/1");
+            task.ContinueWith(
+                t =>
+                {
+                    t.Result.Content.ReadAsStringAsync().ContinueWith(
+                        tt =>
+                        {
+                            Console.WriteLine(tt.Result);
+                        });
+                });
+            GetCustomer2();
+            AddingNew();
+            Console.ReadLine();
+        }
+
+        private static void AddingNew()
+        {
+            var client = new HttpClient();
+            //...
+        }
+
+        private static async void GetCustomer2()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("http://localhost:5000/api/Customers/2");
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+    }
+}
---
+++ RESTSample/src/RESTSample/CustomerRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTSample
+{
+    public static class CustomerRepository
+    {
+        // static (keine Instanz, ist immer da); readonly(kann nicht zugewiesen werden), Dictionary(Map(HashTable)
+        private static readonly Dictionary<int, Customer> s_Customers = new Dictionary<int, Customer>();
+
+        static CustomerRepository()
+        {
+            s_Customers.Add(
+                1,
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Peter Aurin",
+                    LastContact = new DateTime(1999, 12, 5, 10, 30, 0)
+                });
+            s_Customers.Add(
+                2,
+                new Customer
+                {
+                    Id = 2,
+                    Name = "Denis Anderwald",
+                    LastContact = new DateTime(2006, 9, 23, 10, 30, 0)
+                });
+            s_Customers.Add(
+                3,
+                new Customer
+                {
+                    Id = 3,
+                    Name = "Jacob Klimes",
+                    LastContact = new DateTime(2014, 1, 5, 10, 30, 0)
+                });
+            s_Customers.Add(
+                4,
+                new Customer
+                {
+                    Id = 4,
+                    Name = "Gabi Peters"
+                });
+        }
+
+        public static IEnumerable<Customer> GetAll()
+        {
+            return new List<Customer>(s_Customers.Values);
+        }
+
+        public static Customer Get(int id)
+        {
+            //Exception freundlichere Variante
+            Customer customer;
+            s_Customers.TryGetValue(id, out customer);
+            return customer;
+        }
+
+        //CRUD-Operationen
+        public static void Add(Customer c)
+        {
+            s_Customers.Add(c.Id, c);
+        }
+
+        public static void Update(Customer c)
+        {
+            s_Customers[c.Id] = c;
+        }
+
+        public static void Delete(int id)
+        {
+            s_Customers.Remove(id);
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LastContact { get; set; }
+    }
+}
